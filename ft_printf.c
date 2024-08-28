775bca7e8681f7d5e77839c89b461e76fdff13b7
uzanchi <ugozanchi5@gmail.com>
2024-08-28 11:06:25 +0200

    add printf with color

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: uzanchi <uzanchi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/02 16:04:09 by uzanchi           #+#    #+#             */
-/*   Updated: 2024/06/05 15:04:38 by uzanchi          ###   ########.fr       */
+/*   Updated: 2024/08/28 11:04:11 by uzanchi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -60,5 +60,35 @@ int	ft_printf(const char *str, ...)
 			printed_count += putchar_count(str[i++]);
 	}
 	va_end(args);
+	return (printed_count);
+}
+
+/*You need to define the colors in the .h files of your project to use this function*/
+int	ft_printf_color(char color, const char *str, ...)
+{
+	va_list	args;
+	size_t	printed_count;
+	size_t	i;
+
+	i = 0;
+	if (!str)
+		return (-1);
+	va_start(args, str);
+	printed_count = 0;
+	putstr_count(color);
+	while (str[i])
+	{
+		if (str[i] == '%' && str[i + 1] == '\0')
+			return (-1);
+		if (str[i] == '%')
+		{
+			printed_count += display_data(str[++i], args);
+			i++;
+		}
+		else
+			printed_count += putchar_count(str[i++]);
+	}
+	va_end(args);
+	putstr_count("\033[0m");
 	return (printed_count);
 }
